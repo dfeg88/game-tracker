118fea3bbbfb2f3f58914dddef81b4362695b357
Daniel Fegan <daniel.fegan@moneysupermarket.com>
2021-01-21 17:19:31 +0000

    initial commit

---
+++ src/test/java/com/danielfegan/gametracker/GametrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielfegan.gametracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
